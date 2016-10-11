f66d759805d16d5a43b2350f828f3329e57af413
Rodion Zinovyev <rodionz@012.net.il>
2016-10-11 19:56:54 +0300

    Employee BL

--- CRA.BL/EmployeeBL.cs
+++ CRA.BL/EmployeeBL.cs
@@ -4,10 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using CRA.Data;
+using CRA.Dal;
 
 namespace CRA.BL
 {
     class EmployeeBL
     {
+        public Deal_Details ReservationSearch(string carNum)
+        {
+            Deal_Details dd;
+
+            using (var context = new CRA_Context())
+            {
+                dd = (from d in context.Deals
+                      where d.Car.CarNumber == carNum
+                      select d).SingleOrDefault();
+
+            }
+            return dd;
+        }
+
+        public void ReservationClosing(Deal_Details dd)
+        {
+            using (var context = new CRA_Context())
+            {
+                context.Deals.Attach(dd);
+                context.Entry(dd).State = EntityState.Modified;
+                context.SaveChanges();
+
+
+
+            }
+
+
+        }
+
+
     }
 }
