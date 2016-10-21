a34279d7e52f3eade201a0e2cc7d2fb3b3aa8167
Rodion Zinovyev <rodionz@012.net.il>
2016-10-22 01:45:17 +0300

    Go to Sleep

--- CRA/App_Start/RouteConfig.cs
+++ CRA/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CarRental
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "CarSearch", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
