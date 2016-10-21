f524e99e52890072e105ca308880e7b040d41268
Rodion Zinovyev <rodionz@012.net.il>
2016-10-21 19:18:18 +0300

    ViewModel

--- CRA/Models/CarViewModel.cs
+++ CRA/Models/CarViewModel.cs
@@ -2,10 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CRA.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace CRA.Models
 {
     public class CarViewModel
     {
+       
+        public int CarID { get; set; }
+
+        public double Mileage { get; set; }
+       
+        public byte[] Picture { get; set; }
+
+        public bool ProperState { get; set; }
+
+        public string CarNumber { get; set; }
+
+        public int BranchID { get; set; }
+    
+        public int ModelID { get; set; }
+
+
+
+      public CarViewModel(Car_Details domainCarDetails)
+        {
+            this.CarID = domainCarDetails.CarID;
+
+            this.Mileage = domainCarDetails.Mileage;
+
+            this.Picture = domainCarDetails.Picture;
+
+            this.ProperState = domainCarDetails.ProperState;
+
+            this.CarNumber = domainCarDetails.CarNumber;
+
+            this.BranchID = domainCarDetails.BranchID;
+
+            this.ModelID = domainCarDetails.ModelID;
+
+
+
+        }
+
     }
 }
