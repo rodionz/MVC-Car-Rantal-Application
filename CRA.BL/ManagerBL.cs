fb845a5e9d03aa98f47e5dd9952bbe3a187630eb
Rodion Zinovyev <rodionz@012.net.il>
2016-10-21 14:31:33 +0300

    Working on Manager Details

--- CRA.BL/ManagerBL.cs
+++ CRA.BL/ManagerBL.cs
@@ -4,61 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using CRA.Data;
 
 namespace CRA.BL
 {
    public class ManagerBL
     {
 
-        public void AddModel()
+        public void AddModel(Model_Details model)
 
         {
 
         }
 
-        public void UpdateModel()
+        public void UpdateModel(Model_Details model)
         {
 
         }
 
-        public void DeleteModel()
+        public void DeleteModel(Model_Details model)
         {
 
 
         }
 
 
-        public void AddCar()
+        public void AddCar(Car_Details car)
         {
 
         }
 
-        public void UpdateCar()
+        public void UpdateCar(Car_Details car)
         {
 
         }
 
-        public void DeleteCar()
-        {
-
-        }
-
-
-
-
-
-
-        public void AddClient()
-        {
-
-        }
-
-        public void UpdateClient()
-        {
-
-        }
-
-        public void DeleteClient()
+        public void DeleteCar(Car_Details car)
         {
 
         }
@@ -67,18 +48,38 @@ namespace CRA.BL
 
 
 
-        public void AddReservation()
+
+        public void AddClient(Client_Details client)
         {
 
         }
 
-        public void UpdateReservation()
+        public void UpdateClient(Client_Details client)
+        {
+
+        }
+
+        public void DeleteClient(Client_Details client)
         {
 
         }
 
 
-        public void DeleteReservation()
+
+
+
+        public void AddReservation(Deal_Details deal)
+        {
+
+        }
+
+        public void UpdateReservation(Deal_Details deal)
+        {
+
+        }
+
+
+        public void DeleteReservation(Deal_Details deal)
         {
 
         }
