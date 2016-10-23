49c59a00ced1ce5f1bc837a566379b514d1daeef
Rodion Zinovyev <rodionz@012.net.il>
2016-10-23 14:54:55 +0300

    Manager Controller

--- CRA/Controllers/ManagerController.cs
+++ CRA/Controllers/ManagerController.cs
@@ -1,4 +1,6 @@
 ﻿using CarRental.BL;
+using CarRental.Data;
+using CarRental.MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,16 +21,101 @@ namespace CarRental.Controllers
         }
 
 
-        // GET: Manager
+       
         public ActionResult Index()
-        {
+        
+        {       
             return View();
         }
 
-        public ActionResult AddModel()
+
+
+
+        public ActionResult AddModel(CarModelViewModel cmvm)
+        {
+            var originalModel = (Model_Details)cmvm;
+            _manager.AddModel(originalModel);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UpdateModel(CarModelViewModel cmvm)
+        {
+            var originalModel = (Model_Details)cmvm;
+            _manager.UpdateModel(originalModel);
+            return View();
+        }
+
+        public ActionResult DeleteModel(CarModelViewModel cmvm)
+        {
+            var originalModel = (Model_Details)cmvm;
+            _manager.DeleteModel(originalModel);
+            return View();
+        }
+
+        public ActionResult AddCar(CarViewModel cvm)
         {
 
+            var originalCar = (Car_Details)cvm;
+            _manager.AddCar(originalCar);
 
+            return View();
+        }
+
+        public ActionResult UpdateCar(CarViewModel cvm)
+        {
+            var originalCar = (Car_Details)cvm;
+            _manager.UpdateCar(originalCar);
+            return View();
+        }
+
+        public ActionResult DeleteCar(CarViewModel cvm)
+        {
+            var originalCar = (Car_Details)cvm;
+            _manager.DeleteCar(originalCar);
+            return View();
+        }
+
+
+        public ActionResult AddClient(ClientViewModel client)
+        {
+            var originalClient = (Client_Details)client;
+            _manager.AddClient(originalClient);
+            return View();
+        }
+
+        public ActionResult UpdateClient(ClientViewModel client)
+        {
+            var originalClient = (Client_Details)client;
+            _manager.UpdateClient(originalClient);
+            return View();
+        }
+
+        public ActionResult DeleteClient(ClientViewModel client)
+        {
+            var originalClient = (Client_Details)client;
+            _manager.DeleteClient(originalClient);
+            return View();
+        }
+
+        public ActionResult AddDeal(DealViewModel dvm)
+        {
+            var originalDeal = (Deal_Details)dvm;
+            _manager.AddReservation(originalDeal);
+            return View();
+        }
+
+        public ActionResult UpdateDeal(DealViewModel dvm)
+        {
+            var originalDeal = (Deal_Details)dvm;
+            _manager.UpdateReservation(originalDeal);
+            return View();
+        }
+
+        public ActionResult DeleteDeal(DealViewModel dvm)
+        {
+            var originalDeal = (Deal_Details)dvm;
+            _manager.DeleteReservation(originalDeal);
             return View();
         }
     }
