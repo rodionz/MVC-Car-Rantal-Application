33687f2ea4b83461f6c2ea31f6ba4cf3579ec8d1
Rodion Zinovyev <rodionz@012.net.il>
2016-11-07 17:42:49 +0200

    Started to work on Security

--- CRA/Controllers/HomeController.cs
+++ CRA/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using CarRental.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace CarRental.Controllers
 {
@@ -12,6 +14,34 @@ namespace CarRental.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel login)
+        {
+
+            if (Validate(login))
+            {
+                FormsAuthentication.SetAuthCookie(login.Username, false);
+                return Redirect(FormsAuthentication.DefaultUrl);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Login failed.");
+                return View(login);
+            }
+        }
+
+        private bool Validate(LoginViewModel login)
+        {
+            return true;
         }
     }
 }
---
+++ CRA/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarRental.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
