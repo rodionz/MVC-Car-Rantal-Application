1602798764348e9e1059d77c325d3d4672832836
Rodion Zinovyev <rodionz@012.net.il>
2016-11-08 17:51:09 +0200

    Maybe we will work whith Sass

---
+++ CRA/gulpfile.js
@@ -0,0 +1,17 @@
+﻿/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    less = require("gulp-less"),
+    sass = require("gulp-sass"); 
+
+var paths = {
+    webroot: "./wwwroot/"
+};
+
+gulp.task("sass", function () {
+    return gulp.src('Content/Sass/MainSass.scss')
+            .pipe(sass())
+            .pipe(gulp.dest(paths.webroot + '/css'))
+});
