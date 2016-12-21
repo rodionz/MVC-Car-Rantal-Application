f9c305172900a47ceeb43a6158cbc769f27c4a18
Rodion Zinovyev <rodionz@012.net.il>
2016-12-21 23:29:07 +0200

    Some Style tricks

--- CRA/Scripts/MainScript.js
+++ CRA/Scripts/MainScript.js
@@ -1,50 +1,9 @@
-﻿//$(function () {
-     //$(function () {
-
-     //     var GetAllCars = function (url) {
-     //         $.getJSON(url, function (data) {
-
-     //             allcars = data;
-
-              
-
-     //             for(let car of data)
-     //             {
-     //                 let result = ("<li>" + 'CarID: ' + car.CarID + '</br>'
-     //                     + 'Milliage: ' + car.Mileage + '</br>'
-     //                     + 'Car Number: ' + car.CarNumber + "</li></br></br></br>")
-                
-                    
-     //                 var imgSource = '@Url.Action("GetImage")'
-     //                 var img = $('<img/>').attr('src', $.get(imgSource, { modelId: car.ModelID }))
-     //                 .on('load', function () {
-     //                     $('.carlist').append(img)
-     //                 });
+﻿
+$(function () {
 
 
-               
-     //                 $.get(imgSource, { id: data.ModelID })
-                 
-                      
-                   
 
 
-     //                 $('.carlist').append(result);
-
-
-            
-
-                   
-     //             }
-     //         })
-
-     //     }
-
-     //     var allcarslink = '@Url.Action("GettAllCars", "CarSearch")';
-     //     GetAllCars(allcarslink);
-     // });
-
-
-//})
-
+     $( "#datepicker" ).datepicker();
+ } );
 
