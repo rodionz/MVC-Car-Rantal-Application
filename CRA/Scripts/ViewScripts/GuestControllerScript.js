b5e814dcb3b706d250227a00035428f55c47fd84
Rodion Zinovyev <rodionz@012.net.il>
2016-12-25 12:08:33 +0200

    File structure changed

---
+++ CRA/Scripts/ViewScripts/GuestControllerScript.js
@@ -0,0 +1,36 @@
+﻿
+$(function () {
+    $(function () {
+        $("#datepicker").datepicker();
+
+
+
+        
+
+
+        $('#searchbyTransmission').click(function () {
+            console.log('searchbyTransmission')
+        });
+
+        $('#searchbyModel').click(function () {
+            console.log('searchbyModel')
+        });
+
+        $('#searchbyManufacturer').click(function () {
+            console.log('searchbyManufacturer')
+        });
+
+        $('#searchbyDate').click(function () {
+            console.log('searchbyDate')
+        });
+
+        $('#freeSearch').click(function () {
+            console.log('freeSearch')
+        });
+
+    });
+});
+
+
+  
+
