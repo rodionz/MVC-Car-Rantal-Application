76a0bec6a11780b227391678446ec7f20f0087ac
Rodion Zinovyev <rodionz@012.net.il>
2017-01-14 22:28:48 +0200

    car return table

--- CRA/Scripts/ViewScripts/EmployeeScript.js
+++ CRA/Scripts/ViewScripts/EmployeeScript.js
@@ -21,29 +21,33 @@
 
         table.className = "table";
 
-        var row = table.insertRow(0);
+        var header = table.createTHead();
 
-        row.innerHTML = "<th>ID of Deal</th><th>Start Date</th><th>Supposed Return</th><th>Real Return</th><th>Client ID</th><th>Car ID</th>";
+        var row = header.insertRow(0);
+
+        row.innerHTML = "<th>ID of Deal</th><th>Start Date</th><th>Supposed Return</th><th>Real Return</th><th>Client ID</th><th>Car ID</th><th>Deal Closing</th>";
+
+        var body = table.createTBody();
 
         for (var model of  arrayofDeals)
         {
             if (!isNaN(model.RealReturn)) {
 
-                $(table).append("<tr><td>" + model.ID + "</td><td>" + moment(model.StartDate).format('MM/DD/YYYY') + "</td><td>" + moment(model.SupposedReturn).format('MM/DD/YYYY') +
+                $(body).append("<tr><td>" + model.ID + "</td><td>" + moment(model.StartDate).format('MM/DD/YYYY') + "</td><td>" + moment(model.SupposedReturn).format('MM/DD/YYYY') +
                     "</td><td>" + "Car is not returned" + "</td><td>" + model.ClientID + "</td><td>" + model.CarID +
                 "</td><td><button class ='btn btn-xs btn-warning dealclose'>Close the Deal</button></td></tr>")
             
             }
 
             else {
-                      $(table).append("<tr><td>" + model.ID + "</td><td>" + moment(model.StartDate).format('MM/DD/YYYY') + "</td><td>" + moment(model.SupposedReturn).format('MM/DD/YYYY') +
+                      $(body).append("<tr><td>" + model.ID + "</td><td>" + moment(model.StartDate).format('MM/DD/YYYY') + "</td><td>" + moment(model.SupposedReturn).format('MM/DD/YYYY') +
                       "</td><td>" + moment(model.RealReturn).format('MM/DD/YYYY') + "</td><td>" + model.ClientID + "</td><td>" + model.CarID +
-                      "</td></tr>")
+                      "</td><td>Deal is closed</td></tr>")
 
                 }
 
         }
-
+        $(table).DataTable();
         $('.column-one').append(table);
 
         console.log(result);
