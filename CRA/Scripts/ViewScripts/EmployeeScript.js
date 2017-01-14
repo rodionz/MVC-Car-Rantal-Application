f3a4c3fc5c2896ebfd99946cefee723867237e2a
Rodion Zinovyev <rodionz@012.net.il>
2017-01-14 21:12:14 +0200

    many changes

--- CRA/Scripts/ViewScripts/EmployeeScript.js
+++ CRA/Scripts/ViewScripts/EmployeeScript.js
@@ -1,6 +1,6 @@
 ﻿$(function () {
     
-
+    //$('table').DataTable();
     $('footer').addClass('bottomfooter');
 
 
@@ -17,23 +17,24 @@
 
         $('.column-one').empty();
 
-        var table = document.createElement('table');
+      
+         
+      
 
-        table.className = "table table-bordered table-hover";
+        
 
-        var row = table.insertRow(0);
-
-        row.innerHTML = "<th>ID of Deal</th><th>Start Date</th><th>Supposed Return</th><th>Real Return</th><th>Client ID</th><th>Car ID</th>";
+        $('table').append("<th>ID of Deal</th><th>Start Date</th><th>Supposed Return</th><th>Real Return</th><th>Client ID</th><th>Car ID</th>");
 
         for (var model of  arrayofDeals)
         {
 
-            $(table).append("<tr><td>" + model.ID + "</td><td>" + moment(model.StartDate).format('MM/DD/YYYY') + "</td><td>" + moment(model.SupposedReturn).format('MM/DD/YYYY') + "</td><td>"
+            $('table').append("<tr><td>" + model.ID + "</td><td>" + moment(model.StartDate).format('MM/DD/YYYY') + "</td><td>" + moment(model.SupposedReturn).format('MM/DD/YYYY') +
+                "</td><td>"
                 + moment(model.RealReturn).format('MM/DD/YYYY') + "</td><td>" + model.ClientID + "</td><td>" + model.CarID +
-                "</td><td><button class='btn btn-xs btn-warning dealclose'>Close the Deal</button></span></td></tr>")
+                "</td><td>" + isNaN(model.RealReturn) ?  "<button class ='btn btn-xs btn-warning dealclose'>Close the Deal</button></td></tr>" : "</td></tr>")
         }
 
-        $('.column-one').append(table);
+        $('.column-one').append('table');
 
         console.log(result);
     });
