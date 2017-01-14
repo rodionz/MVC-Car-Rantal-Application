0028f908c60992ed647a147016bb2e5162247dbf
Rodion Zinovyev <rodionz@012.net.il>
2017-01-14 21:55:44 +0200

    car return comple

--- CRA/Scripts/ViewScripts/EmployeeScript.js
+++ CRA/Scripts/ViewScripts/EmployeeScript.js
@@ -17,24 +17,34 @@
 
         $('.column-one').empty();
 
-      
-         
-      
+        var table = document.createElement('table');
 
-        
+        table.className = "table";
 
-        $('table').append("<th>ID of Deal</th><th>Start Date</th><th>Supposed Return</th><th>Real Return</th><th>Client ID</th><th>Car ID</th>");
+        var row = table.insertRow(0);
+
+        row.innerHTML = "<th>ID of Deal</th><th>Start Date</th><th>Supposed Return</th><th>Real Return</th><th>Client ID</th><th>Car ID</th>";
 
         for (var model of  arrayofDeals)
         {
+            if (!isNaN(model.RealReturn)) {
 
-            $('table').append("<tr><td>" + model.ID + "</td><td>" + moment(model.StartDate).format('MM/DD/YYYY') + "</td><td>" + moment(model.SupposedReturn).format('MM/DD/YYYY') +
-                "</td><td>"
-                + moment(model.RealReturn).format('MM/DD/YYYY') + "</td><td>" + model.ClientID + "</td><td>" + model.CarID +
-                "</td><td>" + isNaN(model.RealReturn) ?  "<button class ='btn btn-xs btn-warning dealclose'>Close the Deal</button></td></tr>" : "</td></tr>")
+                $(table).append("<tr><td>" + model.ID + "</td><td>" + moment(model.StartDate).format('MM/DD/YYYY') + "</td><td>" + moment(model.SupposedReturn).format('MM/DD/YYYY') +
+                    "</td><td>" + "Car is not returned" + "</td><td>" + model.ClientID + "</td><td>" + model.CarID +
+                "</td><td><button class ='btn btn-xs btn-warning dealclose'>Close the Deal</button></td></tr>")
+            
+            }
+
+            else {
+                      $(table).append("<tr><td>" + model.ID + "</td><td>" + moment(model.StartDate).format('MM/DD/YYYY') + "</td><td>" + moment(model.SupposedReturn).format('MM/DD/YYYY') +
+                      "</td><td>" + moment(model.RealReturn).format('MM/DD/YYYY') + "</td><td>" + model.ClientID + "</td><td>" + model.CarID +
+                      "</td></tr>")
+
+                }
+
         }
 
-        $('.column-one').append('table');
+        $('.column-one').append(table);
 
         console.log(result);
     });
